be01c1a1d1023660ee411950c0205fe9ffb488e1
Christopher Prendergast (V) <christopher_prendergast@apple.com>
2024-10-19 12:44:04 -0700

    Styles neighborhood counts as extruded polygons

--- app.vue
+++ app.vue
@@ -4,6 +4,14 @@ import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import style from "./mapbox/style.json";
 import sites from "./data/sites.json";
+import neighborhoods from "./data/neighborhoods-with-count.json";
+
+
+// Get bounding box for sites and log to console.
+// import { bbox } from "@turf/turf";
+
+// const sitesBbox = bbox(sites);
+// console.log(sitesBbox);
 
 mapboxgl.accessToken = "pk.eyJ1IjoiY3ByZW5kZXJnYXN0IiwiYSI6ImNtMjZhYjBicjA3czQyam9rMm83N3l5cmYifQ.vL3eE67Ua2dk5belzAYurQ";
 
@@ -12,7 +20,17 @@ const mapContainer = ref();
 onMounted(() => {
   const map = new mapboxgl.Map({
     "container": mapContainer.value,
-    "style": style
+    "style": style,
+    "bounds": [
+      -118.6,
+      33.8,
+      -118.1,
+      34.3
+    ],
+    "fitBoundsOptions": {
+      "padding": 25,
+      "pitch": 45
+    }
   });
 
    // Add zoom and rotation controls to the map.
@@ -24,6 +42,10 @@ onMounted(() => {
     // Add sites data after load.
     const sitesSource = map.getSource("sites");
     sitesSource.setData(sites);
+
+    // Add neighborhoods data after load.
+    const neighborhoodsSource = map.getSource("neighborhoods");
+    neighborhoodsSource.setData(neighborhoods);
    });
 
 
